56efa365ba293c895588e23aff4cf530295ad2ec
katharana <124857314+katharana@users.noreply.github.com>
2023-03-20 22:23:51 -0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,109 @@
+// Dom7
+var $$ = Dom7;
+
+// Init App
+var app = new Framework7({
+  	id: 'com.app',
+  	root: '#app',
+	theme: 'auto',
+	view: {
+        pushState: true
+    },
+	cache:false,
+	cacheDuration: 0,
+	modalTitle: 'App Framework7',
+	panel: {
+		swipe: true,
+	},
+  	routes: [
+		{
+		path: '/home/',
+    	url: 'index.html',
+    	name: 'home',
+  		},
+		{
+		path: '/detalles/',
+    	url: 'detalles.html',
+    	name: 'detalles',
+  		},
+		{
+		path: '/descuentos/',
+    	url: 'descuentos.html',
+    	name: 'descuentos',
+  		},
+		{
+		path: '/swiper/',
+    	url: 'swiper.html',
+    	name: 'swiper',
+  		},
+		{
+		path: '/checkout/',
+    	url: 'checkout.html',
+    	name: 'checkout',
+  		}
+		
+	],
+	dialog: {
+		title: 'Diseño Web App',
+		buttonOk: 'Aceptar',
+  	},
+	popup: {
+		closeOnEscape: true,
+		backdrop : false
+	},
+	sheet: {
+		closeOnEscape: true,
+	},
+	popover: {
+		closeOnEscape: true,
+	},
+	actions: {
+		closeOnEscape: true,
+	}
+});
+
+
+
+$$('#btnLogin').on('click', function (e) {
+	e.preventDefault();
+	
+	var $valid = $$('#form-login')[0].checkValidity();
+	if ($valid){
+        $$('#form-login').trigger('reset');	
+        app.loginScreen.close('#modal-login');
+		
+		app.dialog.alert('¡ Bienvenido a la APP !');
+    }
+	
+});
+
+
+
+$$(document).on('click', '#btnCheckout', function (e) {
+	e.preventDefault();
+
+	
+	app.dialog.confirm('¿Seguro desea finalizar su compra?', function () {
+		
+		//app.dialog.alert('Great!');
+		
+		var notification = app.notification.create({
+		  icon: '<i class="material-icons">check</i>',
+		  title: 'Order',
+		  titleRightText: '',
+		  subtitle: '',
+		  text: "Your order has been received.",
+		  closeTimeout: 3000,
+		});
+		notification.open();
+		
+	});
+	
+	
+	
+});
+
+
+
+
+
